ea02c68f30a61b8a8048b7801f3d0433b9adecc9
JavierDelgadoNoaa <JavierDelgado@noaa.gov>
2018-01-17 23:03:08 +0000

    doc

--- multi_track.py
+++ multi_track.py
@@ -1,31 +1,56 @@
+"""
+Wrapper around plot_tracks.py to plot one cycle per image.
+Can be used as a template to wrap other scripts
+"""
+
 from tempfile import mkstemp
 from datetime import datetime, timedelta
 import os
 import shutil
 import itertools
 
+##
+# SETTINGS
+##
+# The config file to use. For each cycle, a copy will be created with new start_date and 
+# end_date parameters.
 src_config_file = "sample.cfg"
+# First cycle to process
 start_date = datetime(year=2005, month=8, day=1, hour=6, minute=0)
+# Last cycle to process
 end_date = datetime(year=2005, month=8, day=5, hour=0, minute=0)
+# Interval in seconds of cycles to process
 interval_seconds = 6 * 3600
 
+
+##
+# LOGIC
+##
 def date_generator():
+    # Generator to get list of all dates to process
     curr_date = start_date
     while curr_date <= end_date:
         yield curr_date
         curr_date += timedelta(seconds=interval_seconds)
 
+
+##
+# MAIN
+##
+
 dates = list(itertools.islice(date_generator(), 0, None))
 print dates
 
 '''
-## easier:
+## Easier, but requires Pandas (which is in my Jet environment but takes forever
+## to load), so lets stick to generator approach
 import pandas
 f = str(interval_seconds) + "S"
 dates = pandas.date_range(start_date, end_date, freq=f)
 #dates.to_pydatetime()
 print list(dates)
 '''
+
 for curr_date in dates:
     fh, abs_path = mkstemp()
     with os.fdopen(fh, "w") as new_file:
